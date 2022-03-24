4e98c6529e96be9241225220f6940afac799610c
Pavla Melegova <pamelego@microsoft.com>
2022-03-24 18:17:59 +0100

    fix

--- Example03-SchemaFiles2/SchemaFilesExample2.cs
+++ Example03-SchemaFiles2/SchemaFilesExample2.cs
@@ -37,69 +37,69 @@ public class SchemaFilesExample2
             {
                 // These types are generated from SchemaFilesExample.fbs
 
-                Owner owner1 = new()
-                {
-                    Name = "John",
-                    //Document = File.ReadAllBytes(@"C:\src\fuzzing\Fuzzing\CatStory.txt"),
-                    //CostOfAllCatsInTotal = 105629.0000999888m
-                };
+                //Owner owner1 = new()
+                //{
+                //    Name = "John",
+                //    //Document = File.ReadAllBytes(@"C:\src\fuzzing\Fuzzing\CatStory.txt"),
+                //    //CostOfAllCatsInTotal = 105629.0000999888m
+                //};
 
-                Owner owner2 = new()
-                {
-                    Name = "Alice",
-                    //Document = File.ReadAllBytes(@"C:\src\fuzzing\Fuzzing\CatStory.txt"),
-                    //CostOfAllCatsInTotal = 165629.0000929888m
-                };
+                //Owner owner2 = new()
+                //{
+                //    Name = "Alice",
+                //    //Document = File.ReadAllBytes(@"C:\src\fuzzing\Fuzzing\CatStory.txt"),
+                //    //CostOfAllCatsInTotal = 165629.0000929888m
+                //};
 
-                var cats = new Cat[9];
+                //var cats = new Cat[9];
 
-                for (int i = 0; i < 9; i++)
-                {
-                    Cat c = new()
-                    {
-                        AgeInYears = (uint)i * 2 + 1,
-                        IsFluffy = i % 2 == 0,
-                        Name = "Pet" + i,
-                        Breed = (CatBreed)i,
-                        Whatever = DateTime.UtcNow.Ticks,
-                        GramsOfFoodPerDay = 33.6 * i + 100,
-                        UniqueIdentfier = "Cat_Id_" + i
-                    };
-                    cats[i] = c;
-                }
+                //for (int i = 0; i < 9; i++)
+                //{
+                //    Cat c = new()
+                //    {
+                //        AgeInYears = (uint)i * 2 + 1,
+                //        IsFluffy = i % 2 == 0,
+                //        Name = "Pet" + i,
+                //        Breed = (CatBreed)i,
+                //        Whatever = DateTime.UtcNow.Ticks,
+                //        GramsOfFoodPerDay = 33.6 * i + 100,
+                //        UniqueIdentfier = "Cat_Id_" + i
+                //    };
+                //    cats[i] = c;
+                //}
 
-                cats[1].Children = new Cat[] { cats[3] };
-                cats[2].Children = new Cat[] { cats[4], cats[8] };
+                //cats[1].Children = new Cat[] { cats[3] };
+                //cats[2].Children = new Cat[] { cats[4], cats[8] };
 
-                owner1.Cats = new List<Cat>();
-                for (int i = 0; i < 5; i++)
-                {
-                    owner1.Cats.Add(cats[i]);
-                }
+                //owner1.Cats = new List<Cat>();
+                //for (int i = 0; i < 5; i++)
+                //{
+                //    owner1.Cats.Add(cats[i]);
+                //}
 
-                owner2.Cats = new List<Cat>();
-                for (int i = 5; i < 9; i++)
-                {
-                    owner2.Cats.Add(cats[i]);
-                }
+                //owner2.Cats = new List<Cat>();
+                //for (int i = 5; i < 9; i++)
+                //{
+                //    owner2.Cats.Add(cats[i]);
+                //}
 
-                CatsAndOwnersContainer container = new()
-                {
-                    cats = cats,
-                };
+                //CatsAndOwnersContainer container = new()
+                //{
+                //    cats = cats,
+                //};
 
-                //Serializer is pregenerated, so no first - run penalty for FlatBufferSerializer.
-                int maxBytes = CatsAndOwnersContainer.Serializer.GetMaxSize(container);
-                byte[] destination = new byte[maxBytes];
-                int bytesWritten = CatsAndOwnersContainer.Serializer.Write(destination, container);
-                File.WriteAllBytes(@"C:\src\fuzzing\Fuzzing2\TestCases\TestCases.txt", destination);
-                Console.WriteLine(bytesWritten);
+                ////Serializer is pregenerated, so no first - run penalty for FlatBufferSerializer.
+                //int maxBytes = CatsAndOwnersContainer.Serializer.GetMaxSize(container);
+                //byte[] destination = new byte[maxBytes];
+                //int bytesWritten = CatsAndOwnersContainer.Serializer.Write(destination, container);
+                //File.WriteAllBytes(@"C:\src\fuzzing\Fuzzing2\TestCases\TestCases.txt", destination);
+                //Console.WriteLine(bytesWritten);
 
-                //FooBarContainer.Serializer.Parse(Encoding.UTF8.GetBytes(stream));
+                CatsAndOwnersContainer.Serializer.Parse(Encoding.UTF8.GetBytes(stream));
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                //Console.WriteLine(ex.Message);
             }
         });
     }
